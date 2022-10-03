453a9a97f8f022be8b5846eeeec027d9caed5d65
Dainler <dainler.ms@gmail.com>
2022-10-03 17:05:19 -0400

    Initial commit

---
+++ src/test/java/com/keystow/AppKeystowTests.java
@@ -0,0 +1,13 @@
+package com.keystow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppKeystowTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
